19503ea12d78eae7a8c994a684fb39f97b5e7641
Aaron Pryce <aapryce@fu-r9-p2.s19.be>
2023-04-27 16:37:04 +0200

    ccc

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: aapryce <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 09:18:44 by aapryce           #+#    #+#             */
-/*   Updated: 2023/04/27 12:35:01 by aapryce          ###   ########.fr       */
+/*   Updated: 2023/04/27 16:23:41 by aapryce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (!n || dest == src)
+		return (dest);
 	i = 0;
 	while (i < n)
 	{
 		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	return ((void *) dest);
+	return (dest);
 }
